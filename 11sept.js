45a6bf3cc2aafbb73cd56dcbd1122dc877ee3d2b
manishisrani <manishisrani09@gmail.com>
2026-09-11 15:18:06 +0530

    Javascript revision day 1

---
+++ 11sept.js
@@ -0,0 +1,17 @@
+
+// //let can adjust with that 
+// let a=9;
+// a=10;
+
+
+// console.log(a);
+
+
+// //let cant handle redeclaration
+// let b=9;
+// let b=92;// not possible 
+
+// //var can adjust with that redeclaration
+
+// var h=9;
+// var h="manish";
