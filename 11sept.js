0cba740cdf5b22481a54d51c873f7f4fc10b7933
manishisrani <manishisrani09@gmail.com>
2026-09-11 15:48:36 +0530

    Updated JavaScript revision

--- 11sept.js
+++ 11sept.js
@@ -1,17 +1,17 @@
 
-// //let can adjust with that 
-// let a=9;
-// a=10;
+//let can adjust with that 
+let a=9;
+a=10;
 
 
-// console.log(a);
+console.log(a);
 
 
-// //let cant handle redeclaration
+//let cant handle redeclaration
 // let b=9;
 // let b=92;// not possible 
 
-// //var can adjust with that redeclaration
+//var can adjust with that redeclaration
 
-// var h=9;
-// var h="manish";
+var h=9;
+var h="manish";
